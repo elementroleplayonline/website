df775fda3bdd4bc26598acf330962644a48bf9fd
elementroleplayonline <102482482+elementroleplayonline@users.noreply.github.com>
2022-03-27 21:36:23 -0400

    Add files via upload

---
+++ Resources/JS/rules.js
@@ -0,0 +1,69 @@
+var Rules = "Hello World!";
+
+var Rules1 = {
+	
+	contents: [
+        { 
+			title:'Rules 1', 
+			texts:[
+                {text:'1.1 You are expected to create a realistic character, No superpowers or unrealistic Scenarios'},
+                {text:'1.2 No RDM under any circumstances/Fail RP.'},
+                {text:'1.3 No chat spam'}
+            ]
+		},
+		{ 
+			title:'Rules 2', 
+			texts:[
+                {text:'2.1 Report players who break the rules'},
+                {text:'2.2 No PropBlock (Block a person near a door with a car).'},
+                {text:'2.3 No weapons in the street, except the reason Roleplay (Walk with a weapon in hand).'},
+                {text:'2.4 No NLR (Back to the place where you died) you have to wait 5 minutes AND wait until the RP scene is over.'}
+            ]
+		},
+		{ 
+			title:'Rules 3', 
+			texts:[
+                {text:'3.1 No Micro Spam (Speak in the microphone to say nothing / music).'},
+                {text:'3.2 No Flood Chat (Write anything in the chat).'},
+                {text:'3.3 No advertising under penalty of heavy punishment.'}
+            ]
+		},
+		{ 
+			title:'Rules 4', 
+			texts:[
+                {text:'4.1 No SpawnKill (Kill players at the point of reappearance).'},
+                {text:'4.2 No FreeShot (shoot the players for no reason).'},
+                {text:'4.3 No PropSpam (Show multiple objects at once).'},
+                {text:'4.4 No MetaGaming (Know the information about the person by looking in the dashboard, for example.).'},
+                {text:'4.5 Improvement of a base (bunker style or fortress) is only tolerated by gangsters, mafia and law enforcement.'},
+                {text:'4.6 No accessories / invisible cameras.'},
+                {text:'4.7 Not allowed to pull out a heavy weapon when someone is watching you armed'},
+                {text:'4.8 When a restart is programmed, the rules still apply!'}
+            ]
+		},
+		{ 
+			title:'Rules 5', 
+			texts:[
+                {text:'5.1 If this is your first connection, put an RP name.'},
+                {text:'5.2 No FreeKill (Killing someone without reason)'},
+                {text:'5.3 No FreePunch / PropPunch (Type a person for no reason Roleplay).'},
+                {text:'5.4 Do not use Wiremod (or any other tool) to crash the server, cause a bug on the server or play in non-Rp mode.'},
+                {text:'5.5 If you die during an RP action, you are excluded from the action that you are not allowed to return even if you are called in support'},
+                {text:'5.6 Please make your identity for your RP character in the CAD/MDT'},
+                {text:'5.7 If you have to leave mid RP, try to notify everyone involved (except if you disconnect or have an emergency, we totally understand, RL IS more important and wifi crapping out happens all the time)'},
+                {text:'5.8 Do not be racist, under any circumstances.'},
+                {text:'5.9 Do not claim to be staff or owner of the server. Staff names will always be the same in server and on discord/teamspeak.'},
+                {text:'5.10 Never give out personal information'},
+                {text:'5.11 Always try to stay in character ingame'}
+            ]
+		},
+		{ 
+			title:'Thank you for taking time to read the rules.', 
+			texts:[
+                {text:'6.1 By understanding these rules this will provide a great experience across the server'},
+                {text:'6.2 If you have any suggestions or feedback regarding anything above please contact a member of staff on discord.'},
+                {text:'6.3 Dont forget to favorite the server to connect back easily!'}
+            ]
+		}
+    ],
+};
